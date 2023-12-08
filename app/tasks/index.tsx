251729883e377d54cfe989eb45ef95e1e8c8a74c
Nicu Sandu <sandu.nicu94@gmail.com>
2023-12-08 14:01:00 +0200

    Added 'No tasks available' message

--- app/tasks/index.tsx
+++ app/tasks/index.tsx
@@ -4,12 +4,15 @@ import React, { memo, useCallback } from "react";
 import TasksTable from "./tasksTable";
 import styles from "./styles.module.scss";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import { IconButton } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import TaskDialog from "./taskDialog";
 import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { tasksSelector } from "@/redux/features/tasks/selectors";
 
 function Tasks() {
   const router = useRouter();
+  const tasks = useSelector(tasksSelector);
 
   const handleClose = useCallback(() => {
     router.push("/");
@@ -27,7 +30,17 @@ function Tasks() {
           <AddCircleIcon color="primary" fontSize="large" />
         </IconButton>
       </header>
-      <TasksTable />
+      {tasks.length ? (
+        <TasksTable />
+      ) : (
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{ opacity: 0.5, textAlign: "center" }}
+        >
+          No tasks available. Please click + button to create a task.
+        </Typography>
+      )}
       <TaskDialog handleClose={handleClose} />
     </div>
   );
